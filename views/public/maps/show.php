b907ec2a04408b0a8acc358146c57b094daa8691
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-03-04 21:11:55 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineMaps/trunk@2278 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/maps/show.php
+++ views/public/maps/show.php
@@ -5,7 +5,7 @@
 <?php 
 
 echo $this->partial('maps/map.phtml',array("layername" => $layername, "serviceaddy" => $serviceaddy, 'proj4js' => $proj4js,
-					"minx" => $bb['minx'] ,'maxx' => $bb['maxx'] ,'miny' => $bb['miny'] ,'maxy' => $bb['maxy'] ,'srs' => $bb['SRS'] 		));
+					"minx" => $minx ,'maxx' => $maxx ,'miny' => $miny ,'maxy' => $maxy ,'srs' => $srs 		));
 
 
 

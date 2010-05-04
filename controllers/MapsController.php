7b811dfdc3b1a9ba40693f6db4caf3e1e713c818
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-05-04 15:01:40 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineMaps/trunk@2500 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/MapsController.php
+++ controllers/MapsController.php
@@ -18,30 +18,6 @@ class NeatlineMaps_MapsController extends Omeka_Controller_Action
 		$id = (!$id) ? $this->getRequest()->getParam('id') : $id;
 		$item = $this->findById($id,"Item");
 
-		$params = array();
-
-		# now we need to retrieve the bounding box and projection ID
-		$params["serviceaddy"] = neatlinemaps_getServiceAddy($item) ;
-		$params["layername"] = neatlinemaps_getLayerName($item) ;
-
-		$capabilitiesrequest = $params["serviceaddy"] . "?request=GetCapabilities" ;
-		$client = new Zend_Http_Client($capabilitiesrequest);
-		$capabilities = new SimpleXMLElement( $client->request()->getBody() );
-		$tmp = $capabilities->xpath("/WMT_MS_Capabilities/Capability//Layer[Name='" . $params["layername"] . "']/BoundingBox");
-		$bb = $tmp[0];
-		$params["minx"] = $bb['minx'] ;
-		$params["maxx"] = $bb['maxx'] ;
-		$params["miny"] = $bb['miny'] ;
-		$params["maxy"] = $bb['maxy'] ;
-		$params["srs"] = $bb['SRS'] ;
-
-		# now we procure the Proj4js form of the projection to avoid confusion with the webpage trying to do
-		# transforms before the projection has been fetched.
-		$client->resetParameters();
-		$proj4jsurl = NEATLINE_SPATIAL_REFERENCE_SERVICE . "/" . strtr(strtolower($params["srs"]),':','/') ."/proj4js/";
-		$client->setUri($proj4jsurl);
-		$params["proj4js"] = $client->request()->getBody();
-
 		$this->view->params = neatlinemaps_assemble_params_for_map($item);
 
 		# now we retrieve any features from other Items that are tagged with prefix:id

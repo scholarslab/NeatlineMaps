168a594e903c2c3e3d2dfb97b8c3f846a030448e
wsg4w <wsg4w@d187d172-3112-0410-8ee7-ad866068e984>
2010-08-25 13:11:43 +0000

    Added basic testing layout
    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineMaps/trunk@3081 d187d172-3112-0410-8ee7-ad866068e984

---
+++ tests/AllTests.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author    "Scholars Lab"
+ * @version   SVN: $Id$
+ * @copyright 2010 The Board and Visitors of the University of Virginia
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @link      http://github.com/scholarslab/bagit
+ */
+
+define('NEATLINE_MAP_DIR', dirname(dirname(__FILE__)));
+
+class NeatlineMap_AllTests extends PHPUnit_Framework_TestSuite
+{
+    public static function suite()
+    {
+        $suite = new NeatlineMap_AllTests('NeatlineMaps Tests');
+        $testCollector = new PHPUnit_Runner_IncludePathTestCollector(
+            array(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cases')
+        );
+
+        $suite->addTestFiles($testCollector->collectTests());
+        return $suite;
+    }
+}
---
+++ tests/NeatlineMaps_IntegrationHelper.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author    "Scholars Lab"
+ * @version   SVN: $Id$
+ * @copyright 2010 The Board and Visitors of the University of Virginia
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @link      http://github.com/scholarslab/bagit
+ */
+
+class NeatlineMaps_IntegrationHelper {
+    const PLUGIN_NAME = 'NeatlineMaps';
+
+    public function setUpPlugin()
+    {
+        $pluginHelper - new Omeka_Test_Helper_Plugin;
+        $this->_addPluginHooksAndFilters($pluginHelper->pluginBroker,
+                self::PLUGIN_NAME);
+    }
+
+    public function _addPluginHooksAndFilters($pluginBroker, $pluginName)
+    {
+         // Set the current plugin so the add_plugin_hook function works
+        $pluginBroker->setCurrentPluginDirName($pluginName);
+
+        add_plugin_hook('install', 'neatlinemaps_install');
+        add_plugin_hook('uninstall', 'neatlinemaps_uninstall');
+        add_plugin_hook('define_routes', 'neatlinemaps_routes');
+        add_plugin_hook('after_upload_file', 'load_geoserver_raster');
+        add_plugin_hook('public_append_to_items_show', 'neatlinemaps_widget');
+        add_plugin_hook('public_theme_header', 'neatlinemaps_header');
+
+        // add plugin filters
+        add_filter("show_item_in_page","neatlinemaps_show_item_in_page");
+        add_filter(array('Form','Item','Item Type Metadata','Background'),
+            "neatlinemaps_background_widget");
+
+    }
+}
+?>

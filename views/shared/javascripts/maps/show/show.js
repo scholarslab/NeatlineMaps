0c489f0a4c6436739620fb2aad990015ab008f45
wsg4w <wsg4w@d187d172-3112-0410-8ee7-ad866068e984>
2010-12-02 16:22:35 +0000

    remove the jquery.noConflict() mode from show.js
    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineMaps/trunk@3527 d187d172-3112-0410-8ee7-ad866068e984

--- views/shared/javascripts/maps/show/show.js
+++ views/shared/javascripts/maps/show/show.js
@@ -12,8 +12,6 @@ if(!Omeka.NeatlineMaps.history) {
 
 Omeka.NeatlineMaps.createMap = function(config) {
 
-    jQuery.noConflict();
-
 	var wgs84 = new OpenLayers.Projection("EPSG:4326");
 	var myproj = new OpenLayers.Projection(config.srs);
 	// var baseproj = new OpenLayers.Projection("EPSG:900913");

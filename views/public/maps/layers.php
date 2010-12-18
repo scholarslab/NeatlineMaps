4dd170bf2cd1b29ec7abe0d4c000786c26c4d285
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-12-18 17:05:00 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/NeatlineMaps/trunk@3614 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/maps/layers.php
+++ views/public/maps/layers.php
@@ -1,5 +1,13 @@
+<div>
 <?php
 		foreach ($layers as $layer) {
-			echo print_r($layer["Name"],true) . "\n";
+			echo print_r($layer,true) . "\n";
 		}
 ?>
+</div>
+<hr/>
+<div>
+<?php 
+echo print_r($layers,true) . "\n";
+?>
+</div>

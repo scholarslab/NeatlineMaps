83f2d3d1a2d76c5d519ac999299792687c997f82
David McClure <david.mcclure@virginia.edu>
2011-07-07 17:53:05 -0400

    everything except for _getField() migrated to new map class

--- libraries/GeoserverMap.php
+++ libraries/GeoserverMap.php
@@ -43,27 +43,140 @@ class GeoserverMap
 
         $this->map = $map;
 
+        $this->layerTitle = $this->_getLayerTitle();
+        $this->serviceAddress = $this->_getServiceAddress();
+        $this->layerName = $this->_getLayerName();
+        $this->dates = $this->_getDates();
+
     }
 
     /**
-     * This calls an action on all plugin objects that pass the predicate.
+     * Get the layer title.
      *
-     * @param mixed $arg An argument to pass to the predicate and action
-     * methods.
-     *
-     * @return array This returns the result of calling the action on the
-     * plugin objects that pass the predicate.
+     * @return string $title The title.
      */
-    function callAll($arg) {
+    protected function _getLaterTitle() {
 
-        $action = $this->action;
+        $title = $this->getField(); // populate parameters here.
 
-        $results = array();
-        foreach ($this->filterPlugins($arg) as $plugin) {
-            $results[] = $plugin->$action($arg);
+        return $title ? $title :
+            get_option('$geoserver_namespace_prefix') . ':' . $this->map->id;
+
+    }
+
+    /**
+     * Get the service address of the server.
+     *
+     * @return string $address The address.
+     */
+    protected function _getServiceAddress() {
+
+        $address = $this->getField(); // populate parameters here.
+
+        return $address ? $address :
+            get_option('neatlinemaps_geoserver_url') . '/wms';
+
+    }
+
+    /**
+     * 
+     *
+     * @return 
+     */
+    protected function _getLayerName() {
+
+        $layername = $this->getField(); // populate parameters here.
+
+        return $layername ? $layername :
+            get_option('$geoserver_namespace_prefix') . ':' . $this->map->id;
+
+    }
+
+    /**
+     * Get dates for the map.
+     *
+     * @return $dates The parsed dates array.
+     */
+    protected function _getDates() {
+
+        $coverages = $this->map
+            ->getElementTextsByElementNameAndSetName( 'Coverage', 'Dublin Core');
+
+        $dates = null;
+
+        // Wretched. But how to condense?
+        if ($coverages) {
+
+            $dates = array();
+            foreach ($coverages as $coverage) {
+
+                $text = str_replace(' ', '', $coverage->text);
+
+                if ($this->_parseDate($text) == 'date') {
+                    $dates['date'] = $text;
+                }
+
+                else if ($this->_parseDate($text) == 'dates') {
+
+                    $dates = explode(';', $text);
+                    foreach ($dates as $chunk) {
+
+                        $chunk = explode('=', $chunk);
+                        switch ($chunk[0]) {
+                            case 'start':
+                                $dates['start'] == $chunk[1];
+                            break;
+                            case 'end':
+                                $dates['end'] == $chunk[1];
+                            break;
+                        }
+
+                    }
+
+                }
+
+            }
+
         }
 
-        return $results;
+        return $dates;
+
+    }
+
+    /**
+     * Parse a coverage text element and see if it is a date,
+     * a date range, or not a date.
+     *
+     * @param string $string The text to evaluate.
+     *
+     * @return string 'date' if the text is a single date;
+     * @return string 'dates' if the text is a date range;
+     * @return string 'notdate' if the text is not a date.
+     */
+    private function _parseDate($string) {
+
+        // Good grief.
+        $result = preg_match('/^([\+-]?\d{4}(?!\d{2}\b))
+            ((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))
+            ?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}
+            |3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)
+            ([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):
+            ?([0-5]\d)?)?)?)?$/', $string) ? 'date' : false;
+
+        $result = preg_match('/^(start|end|[\=\;\-\T\+\d])+$/', $string) ? 'dates' : false;
+
+        return $result;
+
+    }
+
+    /**
+     * 
+     *
+     * @return 
+     */
+    private function _getField() {
+
+
 
     }
 

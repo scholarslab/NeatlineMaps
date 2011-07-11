aaece14dc2a289aaff8cb30ac0118470a82dcfe4
David McClure <david.mcclure@virginia.edu>
2011-07-11 15:33:47 -0400

    openlayers js functional

--- views/shared/javascripts/maps/show/show.js
+++ views/shared/javascripts/maps/show/show.js
@@ -1,1 +1,32 @@
+jQuery(document).ready(function() {
 
+    var map;
+    OpenLayers.ProxyHost = "proxy.cgi?url=";
+
+    map = new OpenLayers.Map("map");
+
+    var base = new OpenLayers.Layer.WMS("OpenLayers WMS",
+        "http://lat.lib.virginia.edu:8080/geoserver2/Falmouth/wms",
+        {layers: "Falmouth:F_likelybuilds"}
+    );
+
+    map.addLayer(base);
+
+    // // allow testing of specific renderers via "?renderer=Canvas", etc
+    // var renderer = OpenLayers.Util.getParameters(window.location.href).renderer;
+    // renderer = (renderer) ? [renderer] : OpenLayers.Layer.Vector.prototype.renderers;
+
+    // var wfs = new OpenLayers.Layer.Vector("States", {
+    //     strategies: [new OpenLayers.Strategy.BBOX()],
+    //     protocol: new OpenLayers.Protocol.WFS({
+    //         url: "http://demo.opengeo.org/geoserver/wfs",
+    //         featureType: "states",
+    //         featureNS: "http://www.openplans.org/topp"
+    //     }),
+    //     renderers: renderer
+    // });
+    // map.addLayer(wfs);
+
+    map.zoomToExtent(new OpenLayers.Bounds(-140.4, 25.1, -44.4, 50.5));
+
+});

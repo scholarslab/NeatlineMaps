131bd631b7e2b707b4ae76bd2d64fe0139a9fbe9
David McClure <david.mcclure@virginia.edu>
2011-08-18 15:26:46 -0400

    scrubbed view folder

--- views/public/maps/public-header.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $header = array('title' => html_escape('Neatline Maps | ' . $subtitle));
-    head($header);
-
-?>
--- views/public/maps/show.php
+++
@@ -1,9 +0,0 @@
-<?php echo $this->partial('maps/public-header.php', array('subtitle' => '')); ?>
-
-<div id="primary">
-
-    <?php echo $this->partial('maps/map.php', array('map_params' => $map_params)); ?>
-
-</div>
-
-<?php foot(); ?>
